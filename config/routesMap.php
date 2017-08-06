2c14b2ded205ccb3c03ca9738e79c7030d1e252a
artyomderyavka <artyom.deryavka@gmail.com>
2017-08-06 18:39:53 +0300

    Update routesMap.php

--- config/routesMap.php
+++ config/routesMap.php
@@ -4,12 +4,6 @@
  * Date: 03.08.2017
  * Time: 14:01
  */
-
-$routesMap = [
-    'cacheFile' => $projectDir . '/var/cache/route.cache', /* required */
-    'routes' => [
-        ['routesFilePath' => $projectDir . '/config/routes.yml', 'routesPrefix' => ''],                                  /*project monolith*/
-        ['routesFilePath' => $projectDir . '/services/target/config/routes.yml', 'routesPrefix' => '/target'],           /*micro-service: target*/
-        ['routesFilePath' => $projectDir . '/services/content/config/routes.yml', 'routesPrefix' => '/content'],         /*micro-service: content*/
-    ]
-];
+$routesMap = array_merge($routesMap, [
+    ['routesFilePath' => __DIR__ . '/routes.yml', 'routesPrefix' => $routesPrefix],  /*project micro-service*/
+]);
