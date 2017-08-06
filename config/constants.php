4290975c31cba81abbd0e4674f9075e83347833b
Artyom Deryavka <deryavka@togethernetworks.com>
2017-08-06 16:29:13 +0000

    added bootstrap to the monolith

--- config/constants.php
+++ config/constants.php
@@ -6,5 +6,6 @@
  */
 const DEBUG = true;
 const TARGET_SERVICE_CLIENT_TRANSPORT = 'local';
+const CONTENT_SERVICE_CLIENT_TRANSPORT = 'local';
 const ROUTES_CACHE_FILE = '/var/cache/route.cache'; /* required */
 const DI_CONTAINER_CACHE_FILE = '/var/cache/container.php'; /* required */
